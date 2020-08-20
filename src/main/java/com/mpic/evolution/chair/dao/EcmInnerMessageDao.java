6896e2afb3e7b76e254f03af4631d32e058805f8
chenxi1280 <chenxi1280@outlook.com>
2020-08-20 15:07:39 +0800

    站内信 逆向工程

--- src/main/java/com/mpic/evolution/chair/dao/EcmInnerMessageDao.java
+++ src/main/java/com/mpic/evolution/chair/dao/EcmInnerMessageDao.java
@@ -1,9 +1,9 @@
 package com.mpic.evolution.chair.dao;
 
-import java.util.List;
-
 import com.mpic.evolution.chair.pojo.dto.EcmInnerMessageDto;
 import com.mpic.evolution.chair.pojo.entity.EcmInnerMessage;
+
+import java.util.List;
 
 public interface EcmInnerMessageDao {
     int deleteByPrimaryKey(Integer pkMessageId);
@@ -13,10 +13,10 @@ public interface EcmInnerMessageDao {
     int insertSelective(EcmInnerMessage record);
 
     EcmInnerMessage selectByPrimaryKey(Integer pkMessageId);
-    
-    List<EcmInnerMessageDto> selectByList(EcmInnerMessage ecmInnerMessage);
 
     int updateByPrimaryKeySelective(EcmInnerMessage record);
 
     int updateByPrimaryKey(EcmInnerMessage record);
+
+    List<EcmInnerMessageDto> selectByList(EcmInnerMessage ecmInnerMessage);
 }
