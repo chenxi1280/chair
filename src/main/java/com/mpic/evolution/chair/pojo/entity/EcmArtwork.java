7a9e3a165ea53e94f5100bf137648638d9cbf9d8
chenxi1280 <chenxi1280@outlook.com>
2020-08-25 10:56:24 +0800

    新的artwork 类 逆向工程

--- src/main/java/com/mpic/evolution/chair/pojo/entity/EcmArtwork.java
+++ src/main/java/com/mpic/evolution/chair/pojo/entity/EcmArtwork.java
@@ -17,13 +17,10 @@ public class EcmArtwork implements Serializable {
     private Integer pkArtworkId;
 
     /**
-     * 作者id
+     * 作者
      */
     private Integer fkUserid;
 
-    /**
-     * 作者名字
-     */
     private String artworkName;
 
     /**
@@ -32,7 +29,7 @@ public class EcmArtwork implements Serializable {
     private String artworkDescribe;
 
     /**
-     * 状态，0草稿，1待审核，2已发布审核通过，3审核不通过禁封，5被删除
+     * 状态，0草稿，1待审核，2审核通过，3，ai审核通过，4，已发布，5被删除
      */
     private Short artworkStatus;
 
@@ -40,6 +37,11 @@ public class EcmArtwork implements Serializable {
      * 作品封面存储位置，绝对路径
      */
     private String logoPath;
+
+    /**
+     * 图片审核状态 0 未审核  1 通过 2 不通过 
+     */
+    private Short logoPathStatus;
 
     private Date lastCreateDate;
 
@@ -49,6 +51,11 @@ public class EcmArtwork implements Serializable {
      * 四字标签
      */
     private String fourLetterTips;
+
+    /**
+     * 审核人
+     */
+    private Integer fkAuditId;
 
     private static final long serialVersionUID = 1L;
 }
