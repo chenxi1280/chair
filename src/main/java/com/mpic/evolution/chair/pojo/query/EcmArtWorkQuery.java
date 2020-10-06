81fa43fc6f585c23bc7f942583362fbc427f636e
SJ <1366314370@qq.com>
2020-10-06 10:18:20 +0800

    fixed

--- src/main/java/com/mpic/evolution/chair/pojo/query/EcmArtWorkQuery.java
+++ src/main/java/com/mpic/evolution/chair/pojo/query/EcmArtWorkQuery.java
@@ -73,4 +73,9 @@ public class EcmArtWorkQuery extends PageQuery {
      *  小程序 搜索文本字段
      */
     private String searchText;
+    
+    /**
+     * 	作品二维码类型 0是预览 4是已发布
+     */
+    private String codeType;
 }
