c4fddd104660ef32fd8a89a7cee2359e05113428
chenxi1280 <chenxi1280@outlook.com>
2020-10-07 18:59:37 +0800

    代码优化

--- src/main/java/com/mpic/evolution/chair/pojo/vo/NodeOptionLocationVO.java
+++ src/main/java/com/mpic/evolution/chair/pojo/vo/NodeOptionLocationVO.java
@@ -10,7 +10,7 @@ import lombok.Data;
  */
 @Data
 public class NodeOptionLocationVO {
-    private double xRatio;
-    private double yRatio;
+    private Double xRatio;
+    private Double yRatio;
     private String  isHide;
 }
