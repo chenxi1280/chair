cfd8506c94ee684e88525790600edaa3b47c204d
SJ <1366314370@qq.com>
2021-05-25 18:38:05 +0800

    success write file at local

--- src/main/java/com/mpic/evolution/chair/service/impl/ExportExcelServiceImpl.java
+++ src/main/java/com/mpic/evolution/chair/service/impl/ExportExcelServiceImpl.java
@@ -18,13 +18,21 @@ import com.mpic.evolution.chair.pojo.vo.ExcelExportVo;
 import com.mpic.evolution.chair.service.ExportExcelService;
 import com.mpic.evolution.chair.util.ExpotExcelUtil;
 import com.mpic.evolution.chair.util.JWTUtil;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.region.Region;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -38,6 +46,18 @@ import java.util.List;
 
 @Service
 public class ExportExcelServiceImpl implements ExportExcelService {
+
+    @Value("${sms.secretId}")
+    private String secretId;// 密钥对
+
+    @Value("${sms.secretKey}")
+    private String secretKey;// 密钥对
+
+    @Value("${sms.appid}")
+    private String appId;// 腾讯短信应用的 SDK AppID
+
+    @Value("${excel.localFilePath}")
+    private String localFilePath;
 
     @Resource
     EcmArtworkBroadcastHistoryDao historyDao;
@@ -64,14 +84,10 @@ public class ExportExcelServiceImpl implements ExportExcelService {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         Long startDate = excelExportVo.getStartDate();
         Long endDate = excelExportVo.getEndDate();
-//        LocalDateTime startDate = LocalDateTime.parse(excelExportVo.getStartDate(),df);
         String sDate = dateFormat.format(new Date(startDate));
-//        LocalDateTime endDate = LocalDateTime.parse(excelExportVo.getEndDate(),df);
         String eDate =  dateFormat.format(new Date(endDate));
-        //从数据库中查询出数据组装成content
-        //String content[][] = new String[list.size()][5];
         //excel元素
-        //TODO 查询userid的全部作品 筛选出免广告的作品id 根据这些id 请求获取这些作品id的播放历史记录 和相关信息
+        // 查询userid的全部作品 筛选出免广告的作品id 根据这些id 请求获取这些作品id的播放历史记录 和相关信息
         EcmArtWorkQuery ecmArtWorkQuery = new EcmArtWorkQuery();
         ecmArtWorkQuery.setFkUserid(userId);
         List<EcmArtworkVo> ecmArtworkVos = ecmArtworkDao.selectArtWorks(ecmArtWorkQuery);
@@ -111,14 +127,21 @@ public class ExportExcelServiceImpl implements ExportExcelService {
         }
         //创建HSSFWorkbook
         HSSFWorkbook wb = ExpotExcelUtil.getHSSFWorkbook(title, headers, content);
-
+        //检查文件是否存在
         //响应到客户端
         try {
-            this.setResponseHeader(response, fileName);
-            OutputStream os = response.getOutputStream();
-            wb.write(os);
-            os.flush();
-            os.close();
+
+//            this.setResponseHeader(response, fileName);
+//            OutputStream os = response.getOutputStream();
+            File sourceFile = new File(localFilePath);
+            String filePath = localFilePath + fileName;
+            FileOutputStream fout = new FileOutputStream(filePath);
+            if (!sourceFile.isDirectory() || !sourceFile.exists()) {
+                sourceFile.mkdir();
+            }
+            wb.write(fout);
+            fout.flush();
+            fout.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -173,6 +196,37 @@ public class ExportExcelServiceImpl implements ExportExcelService {
         data.put("count",recordCount);
         data.put("data",broadcastHistories);
         return ResponseDTO.ok(data);
+    }
+
+    /**
+     * 上传Excel
+     * @param fileNewName
+     * @return
+     */
+    private String uploadExcel(String fileNewName){
+        // 设置bucket所在的区域，比如广州(gz), 天津(tj)
+        Region region = new Region("cq");
+        // 初始化客户端配置
+        ClientConfig clientConfig = new ClientConfig(region);
+        //这里建议设置使用 https 协议
+        clientConfig.setHttpProtocol(HttpProtocol.https);
+        // 初始化秘钥信息
+        // 设置用户属性 secretId和SecretKey
+        COSCredentials cred = new BasicCOSCredentials(secretId,secretKey);
+        // 初始化cosClient
+        COSClient cosClient = new COSClient(cred, clientConfig);
+        // 上传文件的本地目录
+
+        File localFile = new File(localFilePath);
+        // 指定文件将要存放的存储桶
+        // 设置要操作的bucket
+        String bucketName = "examplebucket-1250000000";
+        // 指定文件上传到 COS 上的路径，即对象键。例如对象键为folder/picture.jpg，则表示将文件 picture.jpg 上传到 folder 路径下
+        String key = "excel";
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
+        PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);
+        System.out.println(putObjectResult);
+        return null;
     }
 
     //发送响应流方法
