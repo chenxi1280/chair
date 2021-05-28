5aa1639321663a513537f6fbfd9df7c4537b26fd
SJ <1366314370@qq.com>
2021-05-28 14:24:14 +0800

    fix 21 week mission

--- src/main/java/com/mpic/evolution/chair/pojo/vo/EcmUserHistoryFlowVO.java
+++ src/main/java/com/mpic/evolution/chair/pojo/vo/EcmUserHistoryFlowVO.java
@@ -11,10 +11,10 @@ public class EcmUserHistoryFlowVO extends EcmUserHistoryFlow {
     /**
      * 前端传递的参数 用于查询用户一段时间的下行流量使用记录的起始时间
      */
-    private LocalDateTime startDate;
+    private Long startDate;
 
     /**
      * 前端传递的参数 用于查询用户一段时间的下行流量使用记录的截止时间
      */
-    private LocalDateTime endDate;
+    private Long endDate;
 }
